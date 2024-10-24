cefb840a45a9db0a3e82fd6bdc65de5497983a4f
WURTH\prac.des3 <20213tn032@utez.edu.mx>
2024-10-24 14:37:45 -0600

    [ADD] PAGE $=$

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,6 +6,7 @@ import DashboardPage from "../pages/dashboard/DashboardPage";
 import UserPage from "../pages/users/UserPage";
 import CategoriesPage from "../pages/categories/CategoriesPage";
 import OrdersPage from "../pages/orders/OrderPage";
+import { NotFoundPage } from "../pages/NotFound/NotFoundPage";
 
 const AppRouter = () => {
   return (
@@ -14,7 +15,7 @@ const AppRouter = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/users" element={<UserPage />} />
-      <Route path="*" element={<h1>Not Found</h1>} />
+      <Route path="*" element={<NotFoundPage />} />
       <Route path="/categorias" element={<CategoriesPage />} />
       <Route path="/orders" element={<OrdersPage />} />
     </Routes>
