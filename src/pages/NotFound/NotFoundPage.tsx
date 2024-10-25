d5f1fa7c05b4f5ebf1e10c2ba153bb6c883dd275
WURTH\prac.des3 <20213tn032@utez.edu.mx>
2024-10-25 09:45:16 -0600

    [ADD] This commit refactors the NotFoundPage component by removing unused imports and restructuring the JSX code for improved readability. Additionally, it adds styling to the component using CSS-in-JS. The component now displays a 404 error message and an image, with a button to go back to the previous page.

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,30 +1,58 @@
-import { BorderColor } from "@mui/icons-material";
 import { Button } from "@mui/material";
-import React from "react";
+import { CSSProperties } from "react";
 
 export const NotFoundPage = () => {
+  const goBack = () => {
+    window.history.back();
+  };
+
   return (
-    <div>
-      <h1>¡Oops! Hilo perdido...</h1>
-      <p>Parece que la página que buscas se ha enredado en algún lugar.</p>
-      <p>
-        Mientras la desatamos, ¿por qué no echas un vistazo a nuestras hermosas
-        colecciones de crochet?
-      </p>
-      <Button variant="contained">Regresar </Button>
-      <div style={styles.eclipse as React.CSSProperties}></div>
-      <div style={styles.eclipse as React.CSSProperties}></div>
-      <img src="404.png" alt="404" />
-    </div>
+    <>
+      <div style={styles.container}>
+        <div className="row">
+          <div
+            className="col d-flex flex-column align-items-start justify-content-center"
+            style={{ height: "100vh" }}
+          >
+            <h1 className="title" style={{ fontSize: "45px" }}>
+              ¡Oops! Hilo perdido...
+            </h1>
+            <p>
+              Parece que la página que buscas se ha enredado en algún lugar.
+            </p>
+            <p>
+              Mientras la desatamos, ¿por qué no echas un vistazo a nuestras
+              hermosas colecciones de crochet?
+            </p>
+            <Button onClick={goBack} variant="contained">
+              Regresar
+            </Button>
+          </div>
+          <div className="col d-flex align-items-center justify-content-center">
+            <img
+              src="404.png"
+              alt="404"
+              className="img-fluid"
+              style={styles.img}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-const styles = {
-  eclipse: {
-    width: "500px",
-    height: "500px",
-    borderRadius: "50%",
-    backgroundColor: "white",
-    border: "40px solid #402F2F",
+const styles: { [key: string]: CSSProperties } = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    overflow: "hidden",
+  },
+  img: {
+    maxWidth: "100%",
+    height: "auto",
   },
 };
