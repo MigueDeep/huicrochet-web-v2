6f9f73dde89814c3eaa94703aacea950b060f70d
Miguel Delgado <105407860+MigueDeep@users.noreply.github.com>
2024-11-07 19:46:56 -0600

    [update] modify colors page

--- src/components/colors/ColorsTable.tsx
+++ src/components/colors/ColorsTable.tsx
@@ -1,103 +1,119 @@
 import { useState, useMemo } from "react";
 import {
-    Table,
-    TableHeader,
-    TableColumn,
-    TableBody,
-    TableRow,
-    TableCell,
-    getKeyValue,
-    Chip,
-    Pagination,
-    ButtonGroup,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Chip,
+  Pagination,
+  ButtonGroup,
 } from "@nextui-org/react";
 import ChangeStatus from "../common/ChangesStatus";
+import CreateColorModal from "../../components/colors/CreateColor";
 import EditColorModal from "./EditColor";
 
 const rows = [
-    { key: '1', color: "#FF0000", name: "Rojo", status: "activo" },
-    { key: '2', color: "#00FF00", name: "Verde", status: "deshabilitado" },
-    { key: '3', color: "#0000FF", name: "Azul", status: "activo" },
-    { key: '4', color: "#FFFF00", name: "Amarillo", status: "activo" },
-    { key: '5', color: "#FF00FF", name: "Morado", status: "deshabilitado" },
-    { key: '6', color: "#00FFFF", name: "Cyan", status: "deshabilitado" },
+  { key: "1", color: "#FF0000", name: "Rojo", status: "activo" },
+  { key: "2", color: "#00FF00", name: "Verde", status: "deshabilitado" },
+  { key: "3", color: "#0000FF", name: "Azul", status: "activo" },
+  { key: "4", color: "#FFFF00", name: "Amarillo", status: "activo" },
+  { key: "5", color: "#FF00FF", name: "Morado", status: "deshabilitado" },
+  { key: "6", color: "#00FFFF", name: "Cyan", status: "deshabilitado" },
 ];
 
 const columns = [
-    { key: "color", label: "COLOR" },
-    { key: "name", label: "NOMBRE" },
-    { key: "status", label: "ESTADO" },
-    { key: "actions", label: "ACCIONES" },
+  { key: "color", label: "COLOR" },
+  { key: "name", label: "NOMBRE" },
+  { key: "status", label: "ESTADO" },
+  { key: "actions", label: "ACCIONES" },
 ];
 
 const rowsPerPage = 5;
 
 export default function OrdersTable() {
-    const [page, setPage] = useState(1);
-    const pages = Math.ceil(rows.length / rowsPerPage);
+  const [page, setPage] = useState(1);
+  const pages = Math.ceil(rows.length / rowsPerPage);
 
-    const items = useMemo(() => {
-        const start = (page - 1) * rowsPerPage;
-        const end = start + rowsPerPage;
-        return rows.slice(start, end);
-    }, [page]);
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    return rows.slice(start, end);
+  }, [page]);
 
-    return (
-        <Table
-            aria-label="Example table with dynamic content"
-            bottomContent={
-                <div className="flex w-full justify-center mt-4 pb-4 border-b border-gray-200">
-                    <Pagination
-                        loop
-                        showControls
-                        color="success"
-                        initialPage={1}
-                        page={page}
-                        total={pages}
-                        onChange={(page) => setPage(page)}
-                    />
-                </div>
-            }
-        >
-            <TableHeader columns={columns}>
-                {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-            </TableHeader>
-            <TableBody items={items}>
-                {(item) => (
-                    <TableRow key={item.key}>
-                        {columns.map((column) => (
-                            <TableCell key={column.key}>
-                                {column.key === "color" ? (
-                                    <div className="d-flex gap-2">
-                                        <div style={{ backgroundColor: item.color, width: 20, height: 20, borderRadius: '50%' }}></div>
-                                        <p>{item.color}</p>
-                                    </div>
-                                ) : column.key === "actions" ? (
-                                    <ButtonGroup className="gap-2">
-                                        <EditColorModal id={item.key} />
-                                        <ChangeStatus
-                                            id={item.key}
-                                            initialStatus={item.status === "activo"}
-                                            type="color"
-                                        />
-                                    </ButtonGroup>
-                                ) : column.key === "status" ? (
-                                    <Chip
-                                        className="capitalize"
-                                        size="sm"
-                                        variant="flat"
-                                        color={item.status === "activo" ? "success" : "danger"}
-                                    >
-                                        {item.status}
-                                    </Chip>
-                                ) : (
-                                    getKeyValue(item, column.key)
-                                )}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
-    );
+  return (
+    <>
+    {/* Filter here -- */}
+      <div className="row d-flex justify-content-end">
+        <CreateColorModal />
+      </div>
+      <Table
+        aria-label="Example table with dynamic content"
+        bottomContent={
+          <div className="flex w-full justify-center mt-4 pb-4 border-b border-gray-200">
+            <Pagination
+              loop
+              showControls
+              color="success"
+              initialPage={1}
+              page={page}
+              total={pages}
+              onChange={(page) => setPage(page)}
+            />
+          </div>
+        }
+      >
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={items}>
+          {(item) => (
+            <TableRow key={item.key}>
+              {columns.map((column) => (
+                <TableCell key={column.key}>
+                  {column.key === "color" ? (
+                    <div className="d-flex gap-2">
+                      <div
+                        style={{
+                          backgroundColor: item.color,
+                          width: 20,
+                          height: 20,
+                          borderRadius: "50%",
+                        }}
+                      ></div>
+                      <p>{item.color}</p>
+                    </div>
+                  ) : column.key === "actions" ? (
+                    <ButtonGroup className="gap-2">
+                      <EditColorModal id={item.key} />
+                      <ChangeStatus
+                        id={item.key}
+                        initialStatus={item.status === "activo"}
+                        type="color"
+                      />
+                    </ButtonGroup>
+                  ) : column.key === "status" ? (
+                    <Chip
+                      className="capitalize"
+                      size="sm"
+                      variant="flat"
+                      color={item.status === "activo" ? "success" : "danger"}
+                    >
+                      {item.status}
+                    </Chip>
+                  ) : (
+                    getKeyValue(item, column.key)
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
--- src/pages/colors/ColorsPage.tsx
+++ src/pages/colors/ColorsPage.tsx
@@ -1,14 +1,10 @@
 import ColorsTable from "../../components/colors/ColorsTable";
-import CreateColorModal from "../../components/colors/CreateColor";
 import Layout from "../../components/common/Layout";
 
 const ColorsPage = () => {
   return (
     <Layout title="Colores">
       <div style={styles.container}>
-        <div className="row d-flex justify-content-end">
-          <CreateColorModal />
-        </div>
         <div className="row mt-4">
           <ColorsTable />
         </div>
