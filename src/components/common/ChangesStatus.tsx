eed03c3a6e651872255886464fa20a1ed856af10
Miguel Delgado <105407860+MigueDeep@users.noreply.github.com>
2024-11-18 21:24:31 -0600

    [feat] handle color status working

--- src/components/common/ChangesStatus.tsx
+++ src/components/common/ChangesStatus.tsx
@@ -1,30 +1,36 @@
-import { useState } from 'react';
-import { Tooltip } from '@nextui-org/react';  
-import Switch from '@mui/material/Switch';
+import { useState } from "react";
+import { Tooltip } from "@nextui-org/react";
+import Switch from "@mui/material/Switch";
 
 interface ChangeStatusProps {
   id: string;
   initialStatus: boolean;
-  type: string; 
+  type: string;
+  onStatusChange: (id: string) => void;
 }
 
-export default function ChangeStatus({ id, initialStatus, type }: Readonly<ChangeStatusProps>) {
+export default function ChangeStatus({
+  id,
+  initialStatus,
+  type,
+  onStatusChange,
+}: Readonly<ChangeStatusProps>) {
   const [isActive, setIsActive] = useState(initialStatus);
 
   const handleChangeStatus = () => {
-    console.log(`Changing status of ${type} with id ${id}`);
-    setIsActive(!isActive); 
+    if (type === "color") {
+      setIsActive((prev) => !prev);
+      onStatusChange(id);
+    }
   };
 
   return (
-    <Tooltip 
-      content={isActive ? "Desactivar" : "Activar"}
-    >
-      <Switch 
-        checked={isActive} 
-        onChange={handleChangeStatus} 
-        color="primary" 
-        inputProps={{ 'aria-label': 'primary checkbox' }} 
+    <Tooltip content={isActive ? "Desactivar" : "Activar"}>
+      <Switch
+        checked={isActive}
+        onChange={handleChangeStatus}
+        color="primary"
+        inputProps={{ "aria-label": "primary checkbox" }}
       />
     </Tooltip>
   );
