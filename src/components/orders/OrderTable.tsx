9e74fd3a796bbd1a0816c954a81433b297e31747
BetjaderOD <104954129+BetjaderOD@users.noreply.github.com>
2024-11-23 13:05:40 -0600

    [feat] agregar funcionalidad de búsqueda por ID en la tabla de órdenes; implementar estado de búsqueda y filtrado de datos

--- src/components/orders/OrderTable.tsx
+++ src/components/orders/OrderTable.tsx
@@ -6,17 +6,17 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  getKeyValue,
   Chip,
   Pagination,
 } from "@nextui-org/react";
 import Lottie from "lottie-react";
-import { format } from "@formkit/tempo"
+import { format } from "@formkit/tempo";
 import animationData from "../../utils/animation.json";
 import OrderDetail from "./OrderDetail";
 import { IOrder } from "../../interfaces/IOrder";
 import OrderService from "../../service/OrderService";
-
+import { InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const columns = [
   { key: "order", label: "ORDEN" },
@@ -34,7 +34,8 @@ export default function OrdersTable() {
   const [page, setPage] = useState(1);
   const [ordersData, setOrdersData] = useState<IOrder[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
   const updateOrderState = (id: string, newStatus: string) => {
     setOrdersData((prevOrders) =>
       prevOrders.map((order) =>
@@ -42,7 +43,6 @@ export default function OrdersTable() {
       )
     );
   };
-  
 
   const fetchOrders = useCallback(async () => {
     setIsLoading(true);
@@ -61,86 +61,114 @@ export default function OrdersTable() {
   }, [fetchOrders]);
 
   const pages = Math.ceil(ordersData.length / rowsPerPage);
+  const filteredOrders = useMemo(() => {
+    if (!searchTerm) return ordersData;
+    return ordersData.filter((order) =>
+      order.id.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [searchTerm, ordersData]);
 
   const items = useMemo(() => {
+    if (isLoading) return [];
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
-    return ordersData.slice(start, end);
-  }, [page, ordersData]);
-
+    return filteredOrders.slice(start, end);
+  }, [page, filteredOrders, isLoading]);
 
   return (
-    <Table
-      aria-label="Example table with dynamic content"
-      bottomContent={
-        <div
-          className="
+    <>
+      <div className="col-6 mb-2">
+        <TextField
+          label="Busqueda"
+          placeholder="Ingresa el ID de la orden"
+          variant="outlined"
+          fullWidth
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </div>
+      <Table
+        aria-label="Example table with dynamic content"
+        bottomContent={
+          <div
+            className="
           flex w-full justify-center mt-4 pb-4 border-b border-gray-200
           "
-        >
-          <Pagination
-            loop
-            showControls
-            color="success"
-            initialPage={1}
-            page={page}
-            total={pages}
-            onChange={(page) => setPage(page)}
-          />
-        </div>
-      }
-    >
-      <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody 
-        isLoading={isLoading}
-        loadingContent={
-          <div className="flex justify-center items-center">
-            <Lottie animationData={animationData} style={{ width: 100, height: 100 }} />
+          >
+            <Pagination
+              loop
+              showControls
+              color="success"
+              initialPage={1}
+              page={page}
+              total={pages}
+              onChange={(page) => setPage(page)}
+            />
           </div>
-        }        
-        items={items} emptyContent={"No hay ordenes para mostrar"}>
-         {items.map((order) => (
-          <TableRow key={order.id}>
-            <TableCell>
-            <p className="text-sm font-semibold">
-              {formatId(order.id)}
-            </p>
-            </TableCell>
-            <TableCell>{order.shippingAddress.user.fullName}</TableCell>
-            <TableCell>{order.shippingAddress.user.email}</TableCell>
-            <TableCell>{format(order.orderDate, "long")}</TableCell>
-            <TableCell>{format(order.orderDetails.estimatedDeliveryDate, "long")}</TableCell>
-            <TableCell>
-            <Chip
-              className="capitalize"
-              size="sm"
-              variant="flat"
-              color={renderColor(order.orderState)}
-            >
-              {traduceStatus(order.orderState)}
-            </Chip>
-            </TableCell>
-            <TableCell>
-              <OrderDetail order={order} onOrderUpdate={updateOrderState} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        }
+      >
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          isLoading={isLoading}
+          loadingContent={
+            <div className="flex justify-center items-center">
+              <Lottie
+                animationData={animationData}
+                style={{ width: 100, height: 100 }}
+              />
+            </div>
+          }
+          items={items}
+          emptyContent={"No hay ordenes para mostrar"}
+        >
+          {items.map((order) => (
+            <TableRow key={order.id}>
+              <TableCell>
+                <p className="text-sm font-semibold">{formatId(order.id)}</p>
+              </TableCell>
+              <TableCell>{order.shippingAddress.user.fullName}</TableCell>
+              <TableCell>{order.shippingAddress.user.email}</TableCell>
+              <TableCell>{format(order.orderDate, "long")}</TableCell>
+              <TableCell>
+                {format(order.orderDetails.estimatedDeliveryDate, "long")}
+              </TableCell>
+              <TableCell>
+                <Chip
+                  className="capitalize"
+                  size="sm"
+                  variant="flat"
+                  color={renderColor(order.orderState)}
+                >
+                  {traduceStatus(order.orderState)}
+                </Chip>
+              </TableCell>
+              <TableCell>
+                <OrderDetail order={order} onOrderUpdate={updateOrderState} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
 
-
 const formatId = (id: string) => {
   return id.slice(0, 8);
-}
-
- const formatDate = (date: Date) => {
-  const readable = format(date, "medium")
-  return readable
-}
+};
 
 const traduceStatus = (status: string) => {
   switch (status) {
@@ -153,7 +181,7 @@ const traduceStatus = (status: string) => {
     default:
       return status;
   }
-}
+};
 
 const renderColor = (status: string) => {
   switch (status) {
@@ -166,4 +194,4 @@ const renderColor = (status: string) => {
     default:
       return "danger";
   }
-}
+};
